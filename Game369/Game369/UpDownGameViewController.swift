a611d2529255eab2ef2481b4375b3eed2ad67559
88yhtserof <88yhtserofg@gmail.com>
2025-01-09 18:30:06 +0900

    [FEAT] UpDown: 화면 UI 구성 및 시작 버튼 선택 이벤트 구현 #11

---
+++ Game369/Game369/UpDownGameViewController.swift
@@ -0,0 +1,63 @@
+//
+//  UpDownGameViewController.swift
+//  Game369
+//
+//  Created by 임윤휘 on 1/9/25.
+//
+
+import UIKit
+
+class UpDownGameViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var gameImageView: UIImageView!
+    @IBOutlet var maxTextField: UITextField!
+    @IBOutlet var startButton: UIButton!
+    @IBOutlet var outerStackView: UIStackView!
+    @IBOutlet var gameImageViewHeightConstraint: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureSubviews()
+        configureConstraints()
+    }
+    
+    private func configureSubviews() {
+        titleLabel.text = "UpDown"
+        subtitleLabel.text = "GAME"
+        
+        gameImageView.image = configureRandomGameImage()
+        
+        maxTextField.placeholder = "최대 숫자를 입력하세요"
+        maxTextField.font = .systemFont(ofSize: 16, weight: .bold)
+        maxTextField.textAlignment = .center
+        maxTextField.keyboardType = .numberPad
+        
+        var config = startButton.configuration
+        config?.baseBackgroundColor = .black
+        config?.baseForegroundColor = .white
+        config?.title = "시작하기"
+        startButton.configuration = config
+    }
+    
+    private func configureRandomGameImage() -> UIImage? {
+        let imageNumber = (1...5).randomElement()!
+        let imageName = String(format: "emotion%d", imageNumber)
+        return UIImage(named: imageName)
+    }
+    
+    private func configureConstraints() {
+        view.keyboardLayoutGuide.topAnchor.constraint(greaterThanOrEqualTo: outerStackView.bottomAnchor, constant: 10).isActive = true
+    }
+    
+    @IBAction func viewDidTapped(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func startButtonDidTapped(_ sender: UIButton) {
+        guard let maxNumber = maxTextField.text else { return }
+        view.endEditing(true)
+        print(#function, maxNumber)
+    }
+}
